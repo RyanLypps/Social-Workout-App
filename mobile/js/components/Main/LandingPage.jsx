277bb0393fae307a1b75fe249bb9d82a3599df1d
Asten Fuller <astenhfuller@gmail.com>
2020-04-16 10:34:56 -0700

    Places 2 (#4)
    * added basic list for gym
    
    * styling landing page
    
    * ok
    
    * building schedule page
    
    * got scrollview to work properly
    
    * added user icon to schedule list
    
    * Designed layout on schedule page more. Added Modal. Fixed time to display correctly.
    
    * fixed up modal function
    
    * Drop down working and connected with Redux
    
    * Worked on the drop downs and for the end time.
    
    * finished layout of modal
    
    * working on building the ticker and created database for workout
    
    * building ticker on schedule page
    
    * got ticker to update when workout is posted
    
    * can see who posted workout for each time
    
    * built the schedule details page
    
    * Update LandingPage.jsx
    
    Extra line
    
    * Update package.json
    
    remove link
    
    * Update Styles.jsx
    
    * Update Styles.jsx
    
    * Update Styles.jsx
    
    * Update root.js
    
    * Update ScheduleDetails.jsx
    
    Co-authored-by: Eric Dalmau <dalmaueric29@gmail.com>
    Co-authored-by: Ryan Lypps <ryanlypps@ryans-mbp.attlocal.net>
    Co-authored-by: Ryan Lypps <ryanlypps@Ryans-MacBook-Pro.local>

--- mobile/js/components/Main/LandingPage.jsx
+++ mobile/js/components/Main/LandingPage.jsx
@@ -1,20 +1,46 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
+import { gymList } from '../../MockData';
+import { landingPage } from '../../../styles/Styles'
 
 class LandingPage extends Component {
-    render() {
+  goToSchedulePage = gymId => Actions.schedule({gymId: gymId});
+  gymList() {
+    return (
+      gymList.results.map((gym, index) => {
         return (
-            <View>
+          <TouchableOpacity
+          onPress={() => this.goToSchedulePage(gym.place_id)}
+          key={index+1}
+          >
+            <View key={index} style={{ flexDirection: 'row', flex: 1, borderTopColor: 'white', borderWidth: .5 }}>
+              <Image style={landingPage.photo} source={require('../../../assets/gymPic.png')} />
+              <View style={{ flex: 1 }}>
+                <Text style={{ fontSize: 25, alignSelf: 'center' }}>{gym.name && gym.name.length > 21 ? gym.name.slice(0, 21) + '...' : gym.name}</Text>
+                <Text style={{ fontSize: 15, alignSelf: 'center' }}>{gym.vicinity}</Text>
+              </View>
             </View>
+          </TouchableOpacity>
         )
-    }
+      })
+    )
+  }
+
+  render() {
+    return (
+      <ScrollView style={landingPage.container} scrollEnabled={true}>
+        <View style={{ flex: 1 }}>{this.gymList()}</View>
+      </ScrollView>
+    )
+  }
 }
 
 function mapStoreToProps(store) {
-    return {
-      token: store.login.token || store.register.token
-    }
+  return {
+    token: store.login.token || store.register.token,
   }
-  
+}
+
 export default connect(mapStoreToProps)(LandingPage);
